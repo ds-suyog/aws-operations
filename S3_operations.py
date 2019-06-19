8a28605b802dffbfc7e7ced6995ba0e0d65b24c4
ds-suyog <suyog.sethia@gmail.com>
2019-06-20 00:09:40 +0530

    added code

--- S3_operations.py
+++ S3_operations.py
@@ -1,10 +1,12 @@
 import boto3
+import botocore
 
 # Let's use Amazon S3
 
 class S3Helper:
+    # methods for this module
     pass
-        
+
 # low-level client interface
 s3_client = boto3.client('s3')
 
@@ -42,7 +44,7 @@ def create_bucket(bucket_prefix, s3_connection):
 # # create bucket
 # bucket_name, response = create_bucket(
 # 	bucket_prefix='pythonbucket', 
-# 	s3_connection=s3resource.meta.client)
+# 	s3_connection=s3_resource.meta.client)
 
 # # Output the bucket names by s3_client
 # response = s3_client.list_buckets()
@@ -52,36 +54,49 @@ def create_bucket(bucket_prefix, s3_connection):
 
 bucketname = ""
 
-# Print out bucket names by s3_resource
+# iterating over buckets and respective keys
 print("iterating on buckets:")
-for bucket in s3_resource.buckets.all():
+for bucket in s3_resource.buckets.all():    
     print("bucket name = {}".format(bucket.name))
     bucketname = bucket.name
-
-# Upload a new file
-data = open('test.py', 'rb')
-s3_resource.Bucket(bucketname).put_object(Key='test.py', Body=data)
-
-
-k = s3_resource.Bucket(bucketname).new_key('tmp/')
-k.set_contents_from_string('')
+    #print(bucket.get_available_subresources())
+    for key in bucket.objects.all():
+        print("key.key = {}, size = {} KB".format(key.key, key.size/1024))
 
 
-# for bucket in s3_resource.buckets.all():
-#     print("bucket = {} ".format(bucket))
-#     print(bucket.get_available_subresources())
-#     for key in bucket.objects.all():
-#         print(key.key)
+# # Upload file based on file structure
+# data = open('test.py', 'rb')
+# status = s3_resource.Bucket(bucketname).put_object(Key='code/test.py/', Body=data)
+# print("status = {}".format(status))
 
-k = bucket.new_key('tmp/')
-k.set_contents_from_string('')
+# data = open('office1.jpg', 'rb')
+# status = s3_resource.Bucket(bucketname).put_object(Key='data/office1.jpg/', Body=data)
+# print("status = {}".format(status))
+
+# Download file
+#def download_file(bucketname, s3key, localpath)
+try:
+    s3_resource.Bucket(bucketname).download_file('code/test.py/', 'test.py')
+except botocore.exceptions.ClientError as e:
+    if e.response['Error']['Code'] == "404":
+        print("The object does not exist.")
+    else:
+        raise
+
+key = s3_resource.Bucket(bucketname).get_key('data/office1.jpg/')
+key.get_contents_to_filename('dd.jpg')
+
+# obj = s3_resource.Bucket(bucketname).Object('code/test.py')
+# print("obj type = {}".format(type(obj)))
+
 
 # # Empty bucket
 # for bucket in s3_resource.buckets.all():
 #     bucket = s3_resource.Bucket(bucket.name)
 #     bucket.object_versions.delete()
 
-# # must delete all keys before deleting bucket
+# # # must delete all keys before deleting bucket
+# bucket = s3_resource.Bucket(bucketname)
 # for key in bucket.objects.all():
 #     key.delete()
 # bucket.delete()
