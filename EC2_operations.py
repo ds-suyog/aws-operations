b6e0e05c43ec1654365db594fbe4b2a7a2c590a2
ds-suyog <suyog.sethia@gmail.com>
2019-06-20 01:22:34 +0530

    added ec2 ops code

--- EC2_operations.py
+++ EC2_operations.py
@@ -3,6 +3,14 @@ import boto3
 running_instance = []
 ec2 = boto3.resource('ec2')
 
-for instance in ec2.instances.all():
-	print(instance)
-    print(instance.state['Name'])
+#ami-0b99c7725b9484f9e, t2.micro, mem 1 GiB
+#ec2.create_instances(ImageId='ami-0b99c7725b9484f9e', MinCount=1, MaxCount=5)
+    #InstanceType='t2.micro', KeyName='YOUR_SSH_KEY_NAME',
+
+
+instances = ec2.instances.filter(
+    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+for instance in instances:
+    print(instance.id, instance.instance_type)
+
