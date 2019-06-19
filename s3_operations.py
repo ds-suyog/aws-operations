e27865aedc9d464363e38da5a91920e33f3ceacb
ds-suyog <suyog.sethia@gmail.com>
2019-06-19 16:41:06 +0530

    added s3 code for boto3 usage

--- s3_operations.py
+++ s3_operations.py
@@ -7,7 +7,6 @@ s3 = boto3.resource('s3')
 for bucket in s3.buckets.all():
     print(bucket.name)
 
-# Upload a new file
 data = open('test.jpg', 'rb')
-s3.Bucket('my-bucket').put_object(Key='test.jpg', Body=data)
+s3.Bucket('bucket11gg').put_object(Key='test.jpg', Body=data)
 
