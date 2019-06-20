d20df6102962778095cb3599fe08e9f2da352e55
ds-suyog <suyog.sethia@gmail.com>
2019-06-20 17:55:47 +0530

    provisioning and launching EC2 instance

--- EC2_operations.py
+++ EC2_operations.py
@@ -1,16 +1,35 @@
+import logging
 import boto3
+from botocore.exceptions import ClientError
+import constant
 
 running_instance = []
 ec2 = boto3.resource('ec2')
 
-#ami-0b99c7725b9484f9e, t2.micro, mem 1 GiB
-#ec2.create_instances(ImageId='ami-0b99c7725b9484f9e', MinCount=1, MaxCount=5)
-    #InstanceType='t2.micro', KeyName='YOUR_SSH_KEY_NAME',
+session = boto3.session.Session()
+current_region = session.region_name
+print("current_region = {}".format(current_region))
 
+ec2_c = boto3.client('ec2')
+response = ec2_c.describe_instances()
+print(response)
 
-instances = ec2.instances.filter(
-    Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+def create_ec2_instance(image_id, instance_type, keypair_name):
+    """Provision and launch an EC2 instance
+    """
 
-for instance in instances:
-    print(instance.id, instance.instance_type)
+    # Provision and launch the EC2 instance
+    ec2_client = boto3.client('ec2')
+    try:
+        response = ec2_client.run_instances(ImageId='ami-0b99c7725b9484f9e',
+                                            InstanceType=instance_type,
+                                            KeyName=keypair_name,
+                                            MinCount=1,
+                                            MaxCount=1)
+    except ClientError as e:
+        logging.error(e)
+        return None
+    return response['Instances'][0]
 
+resp = create_ec2_instance('ami-0b99c7725b9484f9e', 't2.micro', constant.KEYPAIR_NAME)
+print(resp)
